b4044ef3c742565638b17ecc3318cc0856c02387
Dhanyaja <dhanyaja2003@gmail.com>
2025-06-25 13:14:20 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,3 @@
-// import React, { useState } from "react";
-// import { Button } from "./components/ui/Button";
-// import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-// import Index from "./pages/Index";
-// import NotFound from "./pages/NotFound";
-// import Dashboard from "./components/dashboard/Dashboard";
-
-// const App = () => {
-//   return (
-//     <div>
-//       <Routes>
-//         <Route path="/" element={<Index />} />
-//         <Route path="*" element={<NotFound />} />
-//       </Routes>
-      
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
